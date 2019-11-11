d2a7a308729a3661ea10de7e5a2f5439da88473e
Saad Manzur <smanzur@uci.edu>
2019-11-10 23:26:25 -0800

    Fix inceptionv1

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import numpy as np
 import cv2 as opencv
 
 
-def get_inception_block(_input, filter_1x1, filter_3x3_reduce, filter_3x3, filter_5x5_reduce, filter_5x5, filter_pool):
+def get_inception_block(_input, filter_1x1,
+                        filter_3x3_reduce, filter_3x3,
+                        filter_5x5_reduce, filter_5x5,
+                        filter_pool, name=None):
     conv1 = keras.layers.Conv2D(filter_1x1, kernel_size=1, activation='relu', padding='same')(_input)
 
     conv3_reduce = keras.layers.Conv2D(filter_3x3_reduce, kernel_size=1, activation='relu', padding='same')(_input)
@@ -15,12 +18,24 @@ def get_inception_block(_input, filter_1x1, filter_3x3_reduce, filter_3x3, filte
     maxpool = keras.layers.MaxPool2D((3, 3), strides=(1, 1), padding='same')(_input)
     maxpool = keras.layers.Conv2D(filter_pool, kernel_size=1, activation='relu', padding='same')(maxpool)
 
-    _output = keras.layers.concatenate([conv1, conv3, conv5, maxpool], axis=3)
+    _output = keras.layers.concatenate([conv1, conv3, conv5, maxpool], axis=3, name=name)
 
     return _output
 
 
-def build_model():
+def get_auxilary_output(_input, name=None):
+
+    auxilary = keras.layers.AvgPool2D((5, 5), strides=(3, 3))(_input)
+    auxilary = keras.layers.Conv2D(128, (1, 1), padding='same', activation='relu')(auxilary)
+    auxilary = keras.layers.Flatten()(auxilary)
+    auxilary = keras.layers.Dense(1024, activation='relu')(auxilary)
+    auxilary = keras.layers.Dropout(0.7)(auxilary)
+    auxilary = keras.layers.Dense(10, activation='softmax', name=name)(auxilary)
+
+    return auxilary
+
+
+def GoogLeNet():
     inputs = keras.Input(shape=(224, 224, 3))
 
     conv1 = keras.layers.Conv2D(64, kernel_size=(7, 7), strides=(2, 2))(inputs)
@@ -29,21 +44,27 @@ def build_model():
     conv2 = keras.layers.Conv2D(192, kernel_size=(3, 3), strides=(1, 1))(pool1)
     pool2 = keras.layers.MaxPool2D((3, 3), strides=(2, 2))(conv2)
 
-    inception1 = get_inception_block(pool2, 64, 96, 128, 16, 32, 32)
-    inception2 = get_inception_block(inception1, 128, 128, 192, 32, 96, 64)
+    inception1 = get_inception_block(pool2, 64, 96, 128, 16, 32, 32, 'inception_3a')
+    inception2 = get_inception_block(inception1, 128, 128, 192, 32, 96, 64, 'inception_3b')
 
     pool3 = keras.layers.MaxPool2D((3, 3), strides=(2, 2))(inception2)
 
-    inception3 = get_inception_block(pool3, 192, 96, 208, 16, 48, 64)
-    inception4 = get_inception_block(inception3, 160, 112, 224, 24, 64, 64)
-    inception5 = get_inception_block(inception4, 128, 128, 256, 24, 64, 64)
-    inception6 = get_inception_block(inception5, 112, 144, 288, 32, 64, 64)
-    inception7 = get_inception_block(inception6, 256, 160, 320, 32, 128, 128)
+    inception3 = get_inception_block(pool3, 192, 96, 208, 16, 48, 64, 'inception_4a')
+
+    auxilary1 = get_auxilary_output(inception3, 'auxilary_1')
+
+    inception4 = get_inception_block(inception3, 160, 112, 224, 24, 64, 64, 'inception_4b')
+    inception5 = get_inception_block(inception4, 128, 128, 256, 24, 64, 64, 'inception_4c')
+    inception6 = get_inception_block(inception5, 112, 144, 288, 32, 64, 64, 'inception_4d')
+
+    auxilary2 = get_auxilary_output(inception6, 'auxilary_2')
+
+    inception7 = get_inception_block(inception6, 256, 160, 320, 32, 128, 128, 'inception_4e')
 
     pool4 = keras.layers.MaxPool2D((3, 3), strides=(2, 2))(inception7)
 
-    inception8 = get_inception_block(inception7, 256, 160, 320, 32, 128, 128)
-    inception9 = get_inception_block(inception8, 384, 192, 384, 48, 128, 128)
+    inception8 = get_inception_block(pool4, 256, 160, 320, 32, 128, 128, 'inception_5a')
+    inception9 = get_inception_block(inception8, 384, 192, 384, 48, 128, 128, 'inception_5b')
 
     avg1 = keras.layers.AvgPool2D((7, 7), strides=(1, 1))(inception9)
     dropout1 = keras.layers.Dropout(0.4)(avg1)
@@ -77,14 +98,15 @@ def get_cifar10_dataset(width, height):
     return x_train, y_train, x_valid, y_valid
 
 
-if __name__ == '__main__':
+def train_with_cifar10():
     x_train, y_train, x_valid, y_valid = get_cifar10_dataset(224, 224)
 
-    model = build_model()
+    model = GoogLeNet()
 
     model.compile(
         loss=['categorical_crossentropy'],
         optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=False),
+        loss_weights=[1.0, 0.3, 0.3],
         metrics=['accuracy']
     )
 
@@ -92,3 +114,8 @@ if __name__ == '__main__':
 
     _ = model.fit_generator(augment.flow(x_train, y_train, 256),
                             validation_data=(x_valid, y_valid), epochs=100)
+
+
+if __name__ == '__main__':
+    model = GoogLeNet()
+    print(model.summary())
